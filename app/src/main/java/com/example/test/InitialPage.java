2eb3fec8c8f612c34270a88ea9072ea745c51bd8
libilu61BB <32815142+libilu61BB@users.noreply.github.com>
2018-12-18 21:56:24 +0800

    添加了打开app的初始页面：由初始页面跳转到“我的日历”页面

---
+++ app/src/main/java/com/example/test/InitialPage.java
@@ -0,0 +1,41 @@
+package com.example.test;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class InitialPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_page);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                gotoPrivatePage();
+            }
+        },1000);
+
+
+    }
+    
+    //点击"我的日历"按钮，进入“我的日历”页面
+    public void gotoPrivatePage()
+    {
+        Intent intent = new Intent(InitialPage.this, PrivatePage.class);
+        startActivity(intent);
+    }
+
+
+}
+
