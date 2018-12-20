5588bcdc4df5201150f54ac99c6bb79d57e37165
Leosword <1203442707@qq.com>
2018-12-20 11:40:05 +0800

    v1.4

--- app/src/main/java/com/example/test/FeedbackPage.java
+++ app/src/main/java/com/example/test/FeedbackPage.java
@@ -2,23 +2,37 @@ package com.example.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.*;
 import android.graphics.Color;
 import android.content.Intent;
 
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class FeedbackPage extends AppCompatActivity {
-    private Button Buttonback;
-    private Button buttonf, buttonu, buttonc;
-    private boolean flagf = false, flagu = false, flagc = false;
+    private int TagNum = 3;
+    private Button Back;
+    private Button[] ButtonTag = new Button[TagNum];
+    private boolean[] TagFlag = new boolean[TagNum];
+    private EditText FeedBack;
+    private Button Submit;
+    private String[] TagContent = {"功能建议", "使用问题", "内容相关"};
+    private boolean ThreadFinishFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback_page);
 
-        Buttonback = findViewById(R.id.ButtonBack);
-        Buttonback.setOnClickListener(new View.OnClickListener(){
+        Back = findViewById(R.id.ButtonBack);
+        Back.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent intent = new Intent();
                 intent.setClass(FeedbackPage.this, PrivatePage.class);
@@ -26,44 +40,87 @@ public class FeedbackPage extends AppCompatActivity {
             }
         });
 
-        buttonf = findViewById(R.id.ButtonFunction);
-        buttonu = findViewById(R.id.ButtonUse);
-        buttonc = findViewById(R.id.ButtonContent);
-        buttonf.setOnClickListener(new View.OnClickListener(){
+        for(int i = 0; i < TagNum; i++){
+            ButtonTag[i] = findViewById(R.id.ButtonTag1 + i);
+            ButtonTag[i].setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    int n = v.getId() - R.id.ButtonTag1;
+                    if(TagFlag[n] == false){
+                        ButtonTag[n].setBackgroundColor(Color.parseColor("#64c6a121"));
+                        TagFlag[n] = true;
+                    }
+                    else{
+                        ButtonTag[n].setBackgroundColor(Color.parseColor("#b9b6b6"));
+                        TagFlag[n] = false;
+                    }
+                }
+            });
+        }
+
+        FeedBack = findViewById(R.id.FeedBack);
+
+        Submit = findViewById(R.id.ButtonSubmit);
+        Submit.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                if(flagf == false){
-                    buttonf.setBackgroundColor(Color.parseColor("#64c6a121"));
-                    flagf = true;
-                }
-                else{
-                    buttonf.setBackgroundColor(Color.parseColor("#b9b6b6"));
-                    flagf = false;
-                }
+                Submit();
             }
         });
-        buttonu.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                if(flagu == false){
-                    buttonu.setBackgroundColor(Color.parseColor("#64c6a121"));
-                    flagu = true;
-                }
-                else{
-                    buttonu.setBackgroundColor(Color.parseColor("#b9b6b6"));
-                    flagu = false;
+    }
+
+    public void Submit(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ThreadFinishFlag = false;
+
+                    String feedback = FeedBack.getText().toString();
+
+                    URL url = new URL("反馈地址");
+                    HttpURLConnection connection =  (HttpURLConnection)url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setConnectTimeout(5000);
+                    connection.setReadTimeout(5000);
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+
+                    Log.i("Connection", feedback);
+                    OutputStream os = connection.getOutputStream();
+                    os.write(feedback.getBytes());
+                    os.flush();
+                    os.close();
+
+                    Log.i("Connection", String.valueOf(connection.getResponseCode()));
+                    if (connection.getResponseCode() == 200) {
+                        String result = StreamToString(connection.getInputStream());
+                        Log.i("Connection", result);
+                    }
+                    else{
+                        Log.i("Connection", "Fail");
+                    }
+                    ThreadFinishFlag = true;
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
-        });
-        buttonc.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                if(flagc == false){
-                    buttonc.setBackgroundColor(Color.parseColor("#64c6a121"));
-                    flagc = true;
-                }
-                else{
-                    buttonc.setBackgroundColor(Color.parseColor("#b9b6b6"));
-                    flagc = false;
-                }
-            }
-        });
+        }).start();
+    }
+
+    public String StreamToString(InputStream is) {
+        //把输入流转换成字符串
+        try {
+            ByteArrayOutputStream Baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = is.read(buffer)) != -1)
+                Baos.write(buffer, 0, len);
+            String result = Baos.toString();
+            is.close();
+            Baos.close();
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/test/RegisterPage.java
+++ app/src/main/java/com/example/test/RegisterPage.java
@@ -20,17 +20,6 @@ import java.io.ByteArrayOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-class User{
-    //存储用户的用户名、密码、昵称、院系
-    public String Username, Password, Nickname, Department;
-    public void SetInformation(String Username, String Password, String Nickname, String Department){
-        this.Username = Username;
-        this.Password = Password;
-        this.Nickname = Nickname;
-        this.Department = Department;
-    }
-}
-
 public class RegisterPage extends AppCompatActivity {
     private Button RegisterButton;
     private Spinner DepartmentSpinner;
@@ -38,7 +27,17 @@ public class RegisterPage extends AppCompatActivity {
     private TextView DepartmentText, UsernameText, NicknameText, PasswordText, Password2Text;
     private User Me = new User();
     private boolean RegisterSucceedFlag = false;
-    private boolean ThreadFinishFlag = false;
+
+    private class User{
+        //存储用户的用户名、密码、昵称、院系
+        public String Username, Password, Nickname, Department;
+        public void SetInformation(String Username, String Password, String Nickname, String Department){
+            this.Username = Username;
+            this.Password = Password;
+            this.Nickname = Nickname;
+            this.Department = Department;
+        }
+    }
 
     private List<String> DepartmentChoice(){
         List<String> data = new ArrayList<>();
@@ -95,8 +94,13 @@ public class RegisterPage extends AppCompatActivity {
                 }
                 else{
                     Me.SetInformation(username, password, nickname, department);
-                    Register();
-                    while(!ThreadFinishFlag);
+                    Register register = new Register();
+                    register.start();
+                    try{
+                        register.join();
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
                     if(RegisterSucceedFlag == false){
                         UsernameText.setText("对不起，此用户名已存在!");
                         UsernameText.setVisibility(View.VISIBLE);
@@ -111,55 +115,49 @@ public class RegisterPage extends AppCompatActivity {
         });
     }
 
-    public void Register(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ThreadFinishFlag = false;
-
-                    JSONObject Json = new JSONObject();  //把数据存成Json格式
-                    Json.put("Username", Me.Username);
-                    Json.put("Password", Me.Password);
-                    Json.put("Department", Me.Department);
-                    Json.put("Nickname", Me.Nickname);
-                    String content = String.valueOf(Json);  //Json格式转成字符串来传输
+    public class Register extends Thread{
+        public void run() {
+            try {
+                JSONObject Json = new JSONObject();  //把数据存成Json格式
+                Json.put("Username", Me.Username);
+                Json.put("Password", Me.Password);
+                Json.put("Department", Me.Department);
+                Json.put("Nickname", Me.Nickname);
+                String content = String.valueOf(Json);  //Json格式转成字符串来传输
 
 
-                    URL url = new URL("https://iknow.gycis.me:8443/updateData/addNewUser");  //不同的请求发送到不同的URL地址，见群里的“后端网页名字设计.docx”
-                    HttpURLConnection connection =  (HttpURLConnection)url.openConnection();
-                    connection.setRequestMethod("POST");
-                    connection.setConnectTimeout(5000);
-                    connection.setReadTimeout(5000);
-                    connection.setDoInput(true);
-                    connection.setDoOutput(true);
+                URL url = new URL("https://iknow.gycis.me:8443/updateData/addNewUser");  //不同的请求发送到不同的URL地址，见群里的“后端网页名字设计.docx”
+                HttpURLConnection connection =  (HttpURLConnection)url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setConnectTimeout(5000);
+                connection.setReadTimeout(5000);
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
 
-                    Log.i("Connection", content);
-                    OutputStream os = connection.getOutputStream();  //打开输出流传输数据
-                    os.write(content.getBytes());
-                    os.flush();
-                    os.close();
+                Log.i("Connection", content);
+                OutputStream os = connection.getOutputStream();  //打开输出流传输数据
+                os.write(content.getBytes());
+                os.flush();
+                os.close();
 
-                    Log.i("Connection", String.valueOf(connection.getResponseCode()));  //如果ResponseCode=200说明和服务器连接正确
-                    if (connection.getResponseCode() == 200) {
-                        //以字符串格式读取服务器的返回内容，Register功能只需返回普通字符串，如果请求的是活动信息则将会返回Json格式的字符串，
-                        //可以用形如JSONObject Json = new JSONObject(String)的语句把字符串转成Json格式
-                        String result = StreamToString(connection.getInputStream());
-                        Log.i("Connection", result);
-                        if(result.equals("username existed"))
-                            RegisterSucceedFlag = false;
-                        else if(result.equals("register succeed"))
-                            RegisterSucceedFlag = true;
-                    }
-                    else{
-                        Log.i("Connection", "Fail");
-                    }
-                    ThreadFinishFlag = true;
-                } catch (Exception e) {
-                    e.printStackTrace();
+                Log.i("Connection", String.valueOf(connection.getResponseCode()));  //如果ResponseCode=200说明和服务器连接正确
+                if (connection.getResponseCode() == 200) {
+                    //以字符串格式读取服务器的返回内容，Register功能只需返回普通字符串，如果请求的是活动信息则将会返回Json格式的字符串，
+                    //可以用形如JSONObject Json = new JSONObject(String)的语句把字符串转成Json格式
+                    String result = StreamToString(connection.getInputStream());
+                    Log.i("Connection", result);
+                    if(result.equals("username existed"))
+                        RegisterSucceedFlag = false;
+                    else if(result.equals("register succeed"))
+                        RegisterSucceedFlag = true;
                 }
+                else{
+                    Log.i("Connection", "Fail");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }).start();
+        }
     }
 
     public String StreamToString(InputStream is) {
