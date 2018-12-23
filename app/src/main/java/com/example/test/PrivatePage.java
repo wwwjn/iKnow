ede9569f9131260a5e6eb5dda4b0ff6dcd086b2b
Leosword <1203442707@qq.com>
2018-12-23 09:12:19 +0800

    v3.0

--- app/src/main/java/com/example/test/PrivatePage.java
+++ app/src/main/java/com/example/test/PrivatePage.java
@@ -11,13 +11,15 @@ import android.util.*;
 import java.util.*;
 
 public class PrivatePage extends AppCompatActivity {
-    private int DateButtonNum = 20;
+    private int DateButtonNum = 28;
     private Button[] DateButton = new Button[DateButtonNum];
     private int CurrentDateId = 0;
     private int CurrentYear, CurrentMonth, CurrentDate;
     private MySql mysql;
     private Cursor cursor;
     private int ShowNum = 0;
+    String[] Week = { "日\n", "一\n", "二\n", "三\n", "四\n", "五\n", "六\n"};
+    int[] MonthDay = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     private void Init(){
         LinearLayout layout = findViewById(R.id.DateViewLayout);
@@ -27,7 +29,7 @@ public class PrivatePage extends AppCompatActivity {
         CurrentMonth = Current.get(Calendar.MONTH);
         CurrentDate = Current.get(Calendar.DATE);
         int CurrentWeekday = Current.get(Calendar.DAY_OF_WEEK);
-        String[] Week = { "日\n", "一\n", "二\n", "三\n", "四\n", "五\n", "六\n"};
+
         for(int i = 0; i < DateButtonNum; i++){
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(getPixelsFromDp(50), LinearLayout.LayoutParams.MATCH_PARENT);
             DateButton[i] = new Button(PrivatePage.this);
@@ -35,7 +37,7 @@ public class PrivatePage extends AppCompatActivity {
                 DateButton[i].setBackgroundColor(Color.parseColor("#64c6a121"));
             else
                 DateButton[i].setBackgroundColor(Color.WHITE);
-            DateButton[i].setText(Week[(CurrentWeekday - 1 + i)%7] + String.valueOf((CurrentDate + i - 1)%31 + 1));
+            DateButton[i].setText(Week[(CurrentWeekday - 1 + i)%7] + String.valueOf(((CurrentDate+i-1)%MonthDay[CurrentMonth]+1)));
             DateButton[i].setId(i);
             DateButton[i].setLayoutParams(params);
             DateButton[i].setOnClickListener(new View.OnClickListener(){
@@ -57,9 +59,12 @@ public class PrivatePage extends AppCompatActivity {
             ViewGroup vg = (ViewGroup) v.getParent();
             vg.removeView(v);
         }
+        int DateNow = (CurrentDate+CurrentDateId-1)%MonthDay[CurrentMonth]+1;
+        int MonthNow = (CurrentDate+CurrentDateId)>MonthDay[CurrentMonth]?(CurrentMonth+1)%12+1:CurrentMonth%12+1;
+        int YearNow = MonthNow<CurrentMonth?CurrentYear+1:CurrentYear;
         ShowNum = 0;
         for(int i = 0; i < 9; i++){
-            cursor = mysql.Query(CurrentYear, CurrentMonth%12+1, (CurrentDate + CurrentDateId - 1)%31+1,2*i+6);
+            cursor = mysql.Query(YearNow, MonthNow, DateNow,2*i+6);
             int num = cursor.getCount();
             for(int j = 0; j < num; j++){
                 cursor.moveToNext();
@@ -104,7 +109,7 @@ public class PrivatePage extends AppCompatActivity {
         setContentView(R.layout.activity_private_page);
         Init();
 
-        /*mysql = new MySql(this);
+        /*MySql mysql = new MySql(this);
         mysql.Insert(1,2018,12,25,8,15,9,45,"这是活动1","主标签",
                 "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐1");
         mysql.Insert(2,2018,12,25,9,15,9,45,"这是活动2","主标签",
@@ -116,7 +121,23 @@ public class PrivatePage extends AppCompatActivity {
         mysql.Insert(5,2018,12,26,9,15,16,45,"这是活动3","主标签",
                 "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐5");
         mysql.Insert(6,2018,12,27,9,15,16,45,"这是活动3","主标签",
-                "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐6");*/
+                "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐6");
+        mysql.Insert(7,2018,12,31,20,15,9,45,"这是活动1","主标签",
+                "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐7");
+        mysql.Insert(8,2019,1,5,9,15,9,45,"这是活动2","主标签",
+                "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐8");
+        mysql.Insert(9,2019,1,5,9,15,16,45,"这是活动3","主标签",
+                "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐9");
+        mysql.Insert(10,2019,1,6,9,15,16,45,"这是活动3","主标签",
+                "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐10");
+        mysql.Insert(11,2019,1,6,9,15,16,45,"这是活动3","主标签",
+                "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐11");
+        mysql.Insert(12,2019,1,7,9,15,16,45,"这是活动3","主标签",
+                "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐12");
+        mysql.Insert(13,2019,1,5,11,15,9,45,"这是活动2","主标签",
+                "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐13");
+        mysql.Insert(14,2019,1,5,16,15,16,45,"这是活动3","主标签",
+                "副标签","活动标签","清华大学","拒绝熬夜组",null,"圣诞聚餐14");*/
     }
 
     //点击"公共日历"按钮，进入“公共日历”页面
